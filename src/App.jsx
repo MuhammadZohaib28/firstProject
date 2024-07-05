3d83e8b0c0434b48f180ee37d2f2d38f286cb79f
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2024-07-05 21:30:37 +0500

    Portfolio Website Markup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainSection from "./components/MainSection";
+import Records from "./components/Records";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <MainSection />
+      <Records />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Muhib.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Muhib = () => {
+  return (
+    <div>
+      <h1>Muhib</h1>
+    </div>
+  )
+}
+
+export default Muhib
